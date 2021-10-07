12c0ae79d3ef822516e16a5cb74ca980cbb45550
Martin Eide <oenesmartin@msn.com>
2021-10-07 13:11:35 +0200

    Create GreetingController.java
    The GreetingController class is the code that handles the HTTP request in the Spring framework. Using mysterious annotations.

---
+++ initial/src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name_but_in_java_world_from_request) {
+        return new Greeting(counter.incrementAndGet(), String.format(template,  name_but_in_java_world_from_request));
+    }
+}
